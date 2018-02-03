d888c2ee29de480813e3ffa66ba08a7e354ad7ad
SIVATHANU0708 <35222125+SIVATHANU0708@users.noreply.github.com>
2018-02-03 19:52:18 +0530

    armstrong.C
    Armstrong or not

--- armstrong.C
+++ armstrong.C
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(0
 {
-int n,multiple=1,digit;
+int n,multiple=1,digit,sum=0;
 printf("enter the number:");
 scanf("%d",&n);
 while(n!=0)
@@ -11,6 +11,12 @@ for(i=1;i<=3;++i)
 {
 multiple=multiple*digits;
 }
+  sum=sum+multiple;
 n=n/10;
 }
-
+  if(sum==n)
+    printf("yes");
+  else
+    printf("no");
+  return 0;
+  }
