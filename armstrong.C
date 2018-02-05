2f8300bb348ec8c2c35ecf2f5aedef362514adf8
SIVATHANU0708 <35222125+SIVATHANU0708@users.noreply.github.com>
2018-02-05 15:49:54 +0530

    armstrong.C
    armstrong or not

--- armstrong.C
+++ armstrong.C
@@ -1,17 +1,13 @@
 #include<stdio.h>
 int main(0
 {
-int n,multiple=1,digit,sum=0;
+int n,,digits,sum=0;
 printf("enter the number:");
 scanf("%d",&n);
 while(n!=0)
 {
 digits=n%10;
-for(i=1;i<=3;++i)
-{
-multiple=multiple*digits;
-}
-  sum=sum+multiple;
+  sum=sum+(digits*digits*digits);
 n=n/10;
 }
   if(sum==n)
